55d474aa30cd59949a99ca2b9064a7fe3a399aa4
ghidadb <167251080+ghidadb@users.noreply.github.com>
2025-08-15 15:21:48 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-2/', 
+  base: '/Portfolio-2/', // must match your repo name exactly
 })
