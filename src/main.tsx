520cf274619a53203b2db7f76ae47d691ae6de24
ghidadb <167251080+ghidadb@users.noreply.github.com>
2025-08-15 15:32:28 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
+import './styles/global.css'
+
+// Providers (these paths match your repo)
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import { ThemeProvider } from './context/ThemeContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <Provider store={store}>
+      <ThemeProvider>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 )
